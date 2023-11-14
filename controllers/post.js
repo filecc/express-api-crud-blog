46be0ed1e64ab96dc1f0953f6691a22c320fb2c2
filecc <fil.gia@live.it>
2023-11-14 15:19:04 +0100

    Add delete button to post view and update posts.json

--- controllers/post.js
+++ controllers/post.js
@@ -89,7 +89,13 @@ function show (req, res) {
     <span>${post.tags.join(", ")}</span>
     <form action="/api/delete/${post.id}" method="POST">
     <input type="hidden" name="id" value="${post.id}">
-        <input style='margin: 1rem 0;' type="submit" value="Cancella questo post">
+    <div style="display: flex; align-items:center; gap: .2rem;">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:20px">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+</svg>
+<input style='margin: 1rem 0;' type="submit" value="Cancella questo post">
+    </div>
+        
     </form>
     <a style='font-weight: bold; display: block; padding: 1rem 0;' href="/posts">Torna alla lista dei post</a>
     
